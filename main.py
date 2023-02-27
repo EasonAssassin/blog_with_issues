bc58be51d0f9f115603ecc51227e47c5f72756c8
sy267129 <sy267129@alibaba-inc.com>
2023-02-27 17:36:17 +0800

    fix main.py

--- main.py
+++ main.py
@@ -294,7 +294,7 @@ def save_issue(issue, me, dir_name=BACKUP_DIR):
         f.write(f"# [{issue.title}]({issue.html_url})\n\n")
         f.write(issue.body)
         if issue.comments:
-            f.write("\n## Comments in issues\n")
+            f.write("\n\n## Comments in issues")
             for c in issue.get_comments():
                 if is_me(c, me):
                     f.write("\n\n---\n\n")
